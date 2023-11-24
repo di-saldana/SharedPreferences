375866c9528e046025598d42a473b194e79aeeb9
Dianelys Saldaña <dsl42@alu.ua.es>
2023-11-24 19:00:04 +0100

    Pref first implementation

--- app/src/main/java/es/ua/eps/sharedpreferences/MainKotlinActivity.kt
+++ app/src/main/java/es/ua/eps/sharedpreferences/MainKotlinActivity.kt
@@ -1,6 +1,8 @@
 package es.ua.eps.sharedpreferences
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Base64
@@ -17,6 +19,8 @@ class MainKotlinActivity : AppCompatActivity() {
             setContentView(root)
 
             button.setOnClickListener {
+                saveSharedPreferences()
+                // setCambios()
                 val intent = Intent(this@MainKotlinActivity, DetalleKotlinActivity::class.java)
                 startActivity(intent)
             }
@@ -28,4 +32,26 @@ class MainKotlinActivity : AppCompatActivity() {
     fun decrypt(input: String?): String {
         return String(Base64.decode(input?.toByteArray(), Base64.DEFAULT))
     }
+
+    fun saveSharedPreferences() {
+        val texto = bindings.editText.text
+        val tamano = bindings.seekBar.progress
+
+        val prefs: SharedPreferences = getSharedPreferences("myPrefs", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString("texto", "TEST")// texto.toString())
+        editor.putInt("tamano", 32) //tamano.toString())
+        // editor.commit();  // wait until ends
+        editor.apply() // do it in background
+    }
+
+    fun setCambios() {
+        // val texto = getPrefs
+        // val value = getPrefs
+
+        // bindings.texto.text = "Texto:" + texto
+        // bindings.tamano.text = "Tamaño:" + value
+    }
+
+
 }
