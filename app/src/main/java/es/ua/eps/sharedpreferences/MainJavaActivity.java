4364aa23dc0d588921f7ea0c43502b7ad73db882
di-saldana <dsl42@alu.ua.es>
2023-12-19 15:10:11 +0100

    Fixed overall layout

--- app/src/main/java/es/ua/eps/sharedpreferences/MainJavaActivity.java
+++ app/src/main/java/es/ua/eps/sharedpreferences/MainJavaActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -23,7 +24,7 @@ public class MainJavaActivity extends AppCompatActivity {
     private TextView tamanoView;
     private Button button;
     private Button buttonCerrar;
-    private Button buttonKotlin;
+    private ImageButton buttonKotlin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
